19def93459d90c924c7bd71ba7d061bf9c498397
Thamarai <tmanalan710@gmail.com>
2024-09-09 21:17:37 +0530

    The signup is done

--- index.js
+++ index.js
@@ -1,93 +1,43 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
 
-const JWT_SECRET = "harkirat123";
+const JWT_SECRET = "thamarai";
 
 const app = express();
+
+const port = 3000;
+
 app.use(express.json());
 
-const users = [];
+let users = [
+  {
+    username: "tm",
+    password: "at",
+  },
+];
 
-function logger(req, res, next) {
-    console.log(req.method + " request came");
-    next();
-}
+app.get("/", (req, res) => {});
 
-app.post("/signup", logger, function(req, res) {
-    const username = req.body.username
-    const password = req.body.password
+app.post("/signup", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  //Checking the user is already in the list
+
+  const checkUser = users.find((user) => user.username === username);
+
+  if (checkUser) {
+    res.send("user already exists");
+  } else {
     users.push({
-        username: username,
-        password: password
-    })
+      username: username,
+      password: password,
+    });
+    res.send("new data");
+  }
+});
+app.post("/signin", (req, res) => {});
 
-    // we should check if a user with this username already exists
+app.get("/details", (req, res) => {});
 
-    res.json({
-        message: "You are signed in"
-    })
-})
-
-app.post("/signin", logger, function(req, res) {
-    const username = req.body.username;
-    const password = req.body.password;
-
-    let foundUser = null;
-
-    for (let i = 0; i < users.length; i++) {
-        if (users[i].username === username && users[i].password === password) {
-            foundUser = users[i]
-        }
-    }
-
-    if (!foundUser) {
-        res.json({
-            message: "Credentials incorrect"
-        })
-        return 
-    } else {
-        const token = jwt.sign({
-            username: "raman"
-        }, JWT_SECRET);
-        res.header("jwt", token);
-
-        res.header("random", "harkirat");
-
-        res.json({
-            token: token
-        })
-    }
-})
-
-function auth(req, res, next) {
-    const token = req.headers.token;
-    const decodedData = jwt.verify(token, JWT_SECRET);
-
-    if (decodedData.username) {
-        req.username = decodedData.username;
-        next()
-    } else {
-        res.json({
-            message: "You are not logged in"
-        })
-    }
-}
-
-
-app.get("/me", logger, auth, function(req, res) {
-    
-    let foundUser = null;
-
-    for (let i = 0; i < users.length; i++) {
-        if (users[i].username === req.username) {
-            foundUser = users[i]
-        }
-    }
-
-    res.json({
-        username: foundUser.username,
-        password: foundUser.password
-    })
-})
-
-app.listen(3000);
+app.listen(port);
