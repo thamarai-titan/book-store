ff1bb55345c1da1a4430c8b3d9be9ec55379fd97
Thamarai <tmanalan710@gmail.com>
2024-09-09 22:31:41 +0530

    the details is not working

--- index.js
+++ index.js
@@ -36,30 +36,54 @@ app.post("/signup", (req, res) => {
     res.send("new data");
   }
 });
-    app.post("/signin", (req, res) => {
-        const username = req.body.username;
-        const password = req.body.password;
+app.post("/signin", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
 
-        const foundUser = users.find(user => user.username === username);
+  const foundUser = users.find((user) => user.username === username);
 
-        if(!foundUser) {
-            res.send("username is wrong");
-        }
-        if(foundUser.password != password){
-            res.send("password is incorrect")
-        }
-        else{
+  if (!foundUser) {
+    res.send("username is wrong");
+  }
+  if (foundUser.password != password) {
+    res.send("password is incorrect");
+  } else {
+    const token = jwt.sign(
+      {
+        username: foundUser.username,
+      },
+      JWT_SECRET
+    );
 
-            const token = jwt.sign({
-                username: foundUser.username
-            }, JWT_SECRET)
+    res.header("jwt", token);
 
-            res.header("jwt", token);
+    res.send("signin successfull");
+  }
+});
+function auth(req, res, next) {
+  const token = req.headers.token;
+  const decodeData = jwt.verify(token, JWT_SECRET);
 
-            res.send("signin successfull")
-        }
-    });
+  if (decodeData.username) {
+    req.username == decodeData.username;
+    next();
+  } else {
+    res.send("you are not signed in");
+  }
+}
+app.get("/details", auth, (req, res) => {
+    const currentuser = req.username;
 
-app.get("/details", (req, res) => {});
+    const foundUser = users.find(user => user.username === currentuser);
+    
+    if (foundUser) {
+      res.json({
+        username: foundUser.username,
+        password: foundUser.password,
+      });
+    } else {
+      res.status(404).send("User not found");
+    }
+});
 
 app.listen(port);
